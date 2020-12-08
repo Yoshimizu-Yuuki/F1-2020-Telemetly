65478a814bcb11677becb5c5a9c79bb5e5db4b56
Yoshimizu-Yuuki <yoshimizuyuuki@gmail.com>
2020-12-08 11:43:09 +0900

    BattleTableに色の追加

--- client/src/pages/BattleTelemetry.tsx
+++ client/src/pages/BattleTelemetry.tsx
@@ -29,65 +29,146 @@ function BattleTelemetry() {
 
   return (
     <div className='container'>
-      {
-        battleTelemetryData.map((el)=>{
-          return (
-            <div className='driver-container'>
-              <div className='speedmeter-container'>
-                <div className='speedmeter'>
-                  <span className='drs'>{el.drs?"DRS":""}</span>
-                  <span className='speed'>{el.speed?el.speed:"-"}</span>
-                   <span className='gear'>{el.gear?el.gear:""}</span>
-                  <span className='ers'>ERS:{el.ersStoreEnergy?Math.round(el.ersStoreEnergy / 40000):"-"}%</span>
-                </div>
-              </div>
-              <div className='tyreinfo-container'>
-                <span className='tyre'>{el.visualTyreCompound?el.visualTyreCompound:"UNKNOWN"}</span>
-                <span className='tyrelap'>{el.tyresAgeLaps? el.tyresAgeLaps + "Lap ago":"No data"}</span>
-                <div className='tyredamage-container'>
-                  <span className='fl'>{el.tyresDamage?el.tyresDamage[2]:"-"}</span>
-                  <span className='fr'>{el.tyresDamage?el.tyresDamage[3]:"-"}</span>
-                  <span className='rl'>{el.tyresDamage?el.tyresDamage[0]:"-"}</span>
-                  <span className='rr'>{el.tyresDamage?el.tyresDamage[1]:"-"}</span>
-                </div>
-              </div>
-              <div className='driverinfo-container'>
-              <div className='position'>{el.carPosition?el.carPosition:"-"}</div>
-              <div className='team'>{el.teamName?el.teamName:"UNKNOWN TEAM"}</div>
-
-                <div className='driver'>
-                   <div className='carnumber'>{el.carNumber?el.carNumber:"-"}</div>
-                  {el.driverName?el.driverName:"UNKNOWN DRIVER"}
-                </div>
-                <div className='lastlap'>{el.lastLapTime?"LASTLAP "+ Math.floor(el.lastLapTime / 60) % 60 + ":" + Math.floor(el.lastLapTime % 60)+":"+Math.floor(el.lastLapTime *1000%1000):"LASTLAP NODATA"}</div>
+      {battleTelemetryData.map((el) => {
+        return (
+          <div className={"driver-container" + " " + el.teamName}>
+            <div className='speedmeter-container'>
+              <div className='speedmeter'>
+                <span className='drs'>{el.drs ? "DRS" : ""}</span>
+                <span className='speed'>{el.speed ? el.speed : "-"}</span>
+                <span className='gear'>{el.gear ? el.gear : ""}</span>
+                <span className='ers'>
+                  ERS:
+                  {el.ersStoreEnergy
+                    ? Math.round(el.ersStoreEnergy / 40000)
+                    : "-"}
+                  %
+                </span>
               </div>
             </div>
-          )
-        })
-      }
+            <div className='tyreinfo-container'>
+              <span className='tyre'>
+                {el.visualTyreCompound ? el.visualTyreCompound : "UNKNOWN"}
+              </span>
+              <span className='tyrelap'>
+                {el.tyresAgeLaps ? el.tyresAgeLaps + "Lap ago" : "No data"}
+              </span>
+              <div className='tyredamage-container'>
+                <span
+                  className='fl'
+                  style={{
+                    backgroundColor:
+                      "hsl(" +
+                      (100 - (el.tyresDamage ? el.tyresDamage[2] : 0)) +
+                      ", 100, 50)"
+                  }}
+                >
+                  {el.tyresDamage ? el.tyresDamage[2] : "-"}
+                </span>
+                <span
+                  className='fr'
+                  style={{
+                    backgroundColor:
+                      "hsl(" +
+                      (100 - (el.tyresDamage ? el.tyresDamage[3] : 0)) +
+                      ", 100, 50)"
+                  }}
+                >
+                  {el.tyresDamage ? el.tyresDamage[3] : "-"}
+                </span>
+                <span
+                  className='rl'
+                  style={{
+                    backgroundColor:
+                      "hsl(" +
+                      (100 - (el.tyresDamage ? el.tyresDamage[0] : 0)) +
+                      ", 100, 50)"
+                  }}
+                >
+                  {el.tyresDamage ? el.tyresDamage[0] : "-"}
+                </span>
+                <span
+                  className='rr'
+                  style={{
+                    backgroundColor:
+                      "hsl(" +
+                      (100 - (el.tyresDamage ? el.tyresDamage[1] : 0)) +
+                      ", 100, 50)"
+                  }}
+                >
+                  {el.tyresDamage ? el.tyresDamage[1] : "-"}
+                </span>
+              </div>
+            </div>
+            <div className='driverinfo-container'>
+              <div className='position'>
+                {el.carPosition ? el.carPosition : "-"}
+              </div>
+              <div className='team'>
+                {el.teamName ? el.teamName : "UNKNOWN TEAM"}
+              </div>
+
+              <div className='driver'>
+                <div className='carnumber'>
+                  {el.carNumber ? el.carNumber : "-"}
+                </div>
+                {el.driverName ? el.driverName : "UNKNOWN DRIVER"}
+              </div>
+              <div className='lastlap'>
+                {el.lastLapTime
+                  ? "LASTLAP " +
+                    (Math.floor(el.lastLapTime / 60) % 60) +
+                    ":" +
+                    Math.floor(el.lastLapTime % 60) +
+                    ":" +
+                    Math.floor((el.lastLapTime * 1000) % 1000)
+                  : "LASTLAP NODATA"}
+              </div>
+            </div>
+          </div>
+        );
+      })}
       <div className='lapinfo-container'>
-        <div>LAP {battleTelemetryData[0]?battleTelemetryData[0].currentLapNumber:"-"}/{battleTelemetryData[0]?battleTelemetryData[0].sessionFullLapNumber:"-"}</div>
+        <div>
+          LAP{" "}
+          {battleTelemetryData[0]
+            ? battleTelemetryData[0].currentLapNumber
+            : "-"}
+          /
+          {battleTelemetryData[0]
+            ? battleTelemetryData[0].sessionFullLapNumber
+            : "-"}
+        </div>
       </div>
       <div className='distance-container'>
         <div>DISTANCE</div>
-    <div>{battleTelemetryData[1]&&battleTelemetryData[1].distance?(battleTelemetryData[1].distance / 1000):"+/-"}</div>
+        <div>
+          {battleTelemetryData[1] && battleTelemetryData[1].distance
+            ? battleTelemetryData[1].distance / 1000
+            : "+/-"}
+        </div>
       </div>
       <div className='drszone-container'>
         <div>DRSZone</div>
-        <div>{battleTelemetryData[1]&&battleTelemetryData[1].drsActivationDistance?
-        Math.round(battleTelemetryData[1].drsActivationDistance):"+/-"}m</div>
+        <div>
+          {battleTelemetryData[1] &&
+          battleTelemetryData[1].drsActivationDistance
+            ? Math.round(battleTelemetryData[1].drsActivationDistance)
+            : "+/-"}
+          m
+        </div>
       </div>
     </div>
   );
 }
 
-function getDeltaTime(distance:number,speed:number):number{
+function getDeltaTime(distance: number, speed: number): number {
   // 現在の車速 / 3,600,000 = 1秒間に何m進むか[y]
   // Distance m / y = 追いつくまでの秒数。
   // 追いつくまでの秒数　×　1秒間に進む距離 = 離れている距離
-  const minuteMeter = (speed?speed:1) / 3600000;
+  const minuteMeter = (speed ? speed : 1) / 3600000;
   const distanceMinute = distance / minuteMeter;
-  return Math.floor(distanceMinute /1000)/1000;
+  return Math.floor(distanceMinute / 1000) / 1000;
   // return distanceMinute;
 }
 
